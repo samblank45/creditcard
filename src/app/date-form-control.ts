dd5a056393c15b6744919579ca048dcc27c6199f
sam <sblan45@gmail.com>
2021-06-06 22:07:19 -0400

    adds Input masking logic for expiration date input

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -2,7 +2,10 @@ import { FormControl } from '@angular/forms';
 
 export class DateFormControl extends FormControl{
   setValue(value: string, options: any) {
-    console.log(value);
-    super.setValue(value + '*', options);
+    if (value.length === 2) {
+      super.setValue(value + '/', {...options, emitModelToViewChange: true});
+      return;
+    }
+    super.setValue(value, {...options, emitModelToViewChange: true});
   }
 }
